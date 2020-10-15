436361f842497e7525df9f8c0ab4c56aad1f3b73
Kevin Hernandez <kevin.hernandez1802@gmail.com>
2020-10-14 20:23:27 -0500

    Updates

---
+++ precache-manifest.d15870b0ec7acb6cce08ed3132923e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a75f6008386f79ea385a66542580b9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "237e87211ef304053663",
+    "url": "/static/css/main.4a874138.chunk.css"
+  },
+  {
+    "revision": "ff459f3566fbd3d9f8d2",
+    "url": "/static/js/2.7f004a2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f004a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237e87211ef304053663",
+    "url": "/static/js/main.ef477f07.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
